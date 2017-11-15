e4f95a719a526c6da57438d31ea8e8a6e0fd53f9
4015b17 <4015b17@icaco.ece.uprm.edu>
2017-11-15 18:31:41 -0400

    Change name

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Kevin Sanchez Lorenzo");
+		JFrame myFrame = new JFrame("Kevin Sanchez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
 		myFrame.setSize(200, 200);
